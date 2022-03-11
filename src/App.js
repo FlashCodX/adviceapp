64dc97d01bf291c3703353ffabd374272bb1e6a3
FlashCodX <luis.pinto.trading@gmail.com>
2022-03-11 11:11:25 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+const image_btn = require('./images/icon-dice.svg').default
+const divider = require('./images/pattern-divider-desktop.svg').default
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+
+    const [advice, updateAdvice] = useState('')
+    const [adviceNumber, updateAdviceNumber] = useState(1)
+    const [disabled, updateDisabled] = useState(true)
+
+    useEffect(() => {generateAdvice()}, [])
+
+
+    const generateAdvice = async () => {
+        updateDisabled(true)
+        const data = await fetch('https://api.adviceslip.com/advice')
+        const json = await data.json();
+        updateAdviceNumber(json['slip']['id'])
+        updateAdvice(json['slip']['advice'])
+        setTimeout(()=>updateDisabled(false),2000)
+    }
+    return <main>
+        <h1>Advice #{adviceNumber}</h1>
+        <q>{advice}</q>
+        <img src={divider} alt="divider"/>
+        <button disabled={disabled} onClick={() => generateAdvice()}>
+            <img src={image_btn} alt="button"/>
+        </button>
+    </main>
 }
-
-export default App;
